3e879dc4274654541e551de71e83f85531b87669
obijuan <juan@iearobotics.com>
2013-05-24 12:22:52 +0200

    Initial commit

---
+++ firmware/FingerServer/FingerServer.ino
@@ -0,0 +1,27 @@
+#include <Servo.h> 
+
+//-- Only needed for the skymega board
+//-- If using arduino, it can be commented
+#include <skymega.h>
+
+Servo myservo;  
+  
+void setup() 
+{ 
+  myservo.attach(SERVO2); 
+} 
+ 
+ 
+void loop() 
+{ 
+  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
