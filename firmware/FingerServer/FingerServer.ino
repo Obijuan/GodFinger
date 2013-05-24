07fa2a237f7558b2ba269c5df536770bb7a1c7be
obijuan <juan@iearobotics.com>
2013-05-24 15:44:42 +0200

    Position command implemented

--- firmware/FingerServer/FingerServer.ino
+++ firmware/FingerServer/FingerServer.ino
@@ -4,24 +4,139 @@
 //-- If using arduino, it can be commented
 #include <skymega.h>
 
-Servo myservo;  
+//-- Definition for the frames
+#define FRAME_HEADER ':'
+#define FRAME_POS 'P'     //-- Position command
+
+//-- Global variables
+Servo s[8];     //-- Servos
+char s_index=0; //-- Servo index
+int ang = 0;    //-- Servo angle
   
 void setup() 
 { 
-  myservo.attach(SERVO2); 
+  // Configure the skymega led
+  pinMode(LED, OUTPUT);
+  
+  //-- Configure the serial port
+  Serial.begin(115200);
+  
+  //-- Configure the servos
+  //-- For arduino just write the pin number instead of the
+  //-- labels SERVOx
+  s[0].attach(SERVO1);
+  s[1].attach(SERVO2);
+  s[2].attach(SERVO3);
+  s[3].attach(SERVO4);
+  
+  //-- Initial servo pos
+  for (int i=0; i<4; i++)
+    s[i].write(0 + 90);
+  
+  //-- Turn on the led
+  digitalWrite(LED, OFF);
+  
+  Serial.print("Ready!\n");
 } 
  
- 
+//-- For reading the serial input
+char serial_char;
+
+//-- Buffer for storing the received commands
+#define BUFSIZE 8
+char buffer[BUFSIZE+1];
+int buflen = 0;
+
+bool cmd_ready=false; 
+
+void read_frame()
+{
+  if (Serial.available() && buflen < BUFSIZE) {
+    
+    //-- Read the car
+    serial_char = Serial.read();
+    
+    //-- Detect blank caracters. They are interpreted as the end of a command    
+    if (serial_char == ' ' || serial_char == '\r' || serial_char == '\n') {
+      //Serial.print("Blank\n");
+      
+      //-- Store the end of string
+      buffer[buflen]=0;
+      
+      //-- Now there is a command ready to be processed!
+      if (buflen>0)
+        cmd_ready = true;
+    }
+      
+    //-- Normal character: store it in the buffer  
+    else {
+      buffer[buflen]=serial_char;
+      buflen++;
+      //Serial.print("OK\n");
+    }
+  }
+}
+
+void process_cmd()
+{
+  //-- Parse the command
+  //-- First check if the header is ok
+  if (buffer[0] != FRAME_HEADER)
+    return;  //-- of the Jedi
+  
+  //-- Read the servo number
+  s_index = buffer[1] - '0' -1;
+  if (s_index < 0  ||  s_index > 8)
+    return;  //-- of the Jedi
+  
+  //-- Pos command
+  if (buffer[2] != FRAME_POS)
+    return;  //-- of the Jedi
+  
+  //-- Get the angular value
+  ang = strtol(&buffer[3],NULL,10);
+   
+  //-- Pos the servo 
+  s[s_index].write(ang + 90);
+
+  
+  /*if (s_index == 2) 
+    digitalWrite(LED, ON);
+  else
+    digitalWrite(LED, OFF);
+    */
+  
+  //-- Debug...
+  Serial.print("CMD: ");
+  Serial.print(buffer);
+  Serial.print("---\n");
+  
+  Serial.print("Servo: ");
+  Serial.print(s_index);
+  Serial.print("---\n");
+}
+
+
+
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
+  
+  //-- Task: Read the information from the serial port
+  read_frame();
+  
+  //-- If there is a command ready or the buffer is full
+  //-- process the command!!
+  if (cmd_ready || buflen==BUFSIZE) {
+    
+    //-- Process the command
+    process_cmd();
+    
+    //-- Command processed!
+    cmd_ready=false;
+    buflen=0;
+  }
+  
 } 
+
+
+
