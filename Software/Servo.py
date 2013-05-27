3a90c035284e3e3b16dac77e14df356437584865
obijuan <juan@iearobotics.com>
2013-05-27 11:08:09 +0200

    set_pos() function waits until the servo reaches the target pos (based on stimated time)

--- Software/Servo.py
+++ Software/Servo.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import time
+
+#-- Servo angular speed, in deg/seg (Futaba 3003)
+ServoW = 260.0
+
 class Servo(object):
   """Servo class. For accessing to all the Servos"""
   
@@ -19,11 +24,24 @@ class Servo(object):
     """Set the angular servo pos. The pos is an integer number
        in the range [-90 ,90] """
        
+    #-- TODO: check that pos in in the range [-90,90]   
+       
     #-- Build the frame   
     frame = ":" + str(self.dir) + "P" + str(pos) + " "
+    
+    #-- Debug
     print frame
     
     #-- Send the frame
     self.sp.write(frame)
+    
+    #-- Wait some time, for the servo to reach the target position
+    #-- Stimated time:
+    stime = abs(pos - self._pos) / ServoW
+    #print "Time: {0}".format(stime)  #-- Debug
+    time.sleep(stime)
+    
+    #-- Store the current servo pos
+    self._pos = pos
     
     
